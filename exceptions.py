57b2a914b2af447d4cdad13963eb555e7547b28e
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2023-12-14 17:31:30 +0300

    123

--- exceptions.py
+++
@@ -1,3 +0,0 @@
-class ParserFindTagException(Exception):
-    """Вызывается, когда парсер не может найти тег."""
-    pass
---
+++ src/constants.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEP_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+EXPECTED_STATUS = {
+    'A': ['Active', 'Accepted'],
+    'D': ['Deferred'],
+    'F': ['Final'],
+    'P': ['Provisional'],
+    'R': ['Rejected'],
+    'S': ['Superseded'],
+    'W': ['Withdrawn'],
+    '': ['Draft', 'Active'],
+}
